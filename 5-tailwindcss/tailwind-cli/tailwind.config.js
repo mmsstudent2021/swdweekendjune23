88c74382c3c3ca09b4e5c54044d9a88fdad07615
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-09-30 12:16:05 +0630

    dark mode

--- 5-tailwindcss/tailwind-cli/tailwind.config.js
+++ 5-tailwindcss/tailwind-cli/tailwind.config.js
@@ -1,12 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
+  darkMode: 'class',
   content: [
     "./src/*.{html,js}",
 
 ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+
+        pink: {
+          100: "#fedeeb",
+          200: "#febed7",
+          300: "#fd9dc4",
+          400: "#fd7db0",
+          500: "#fc5c9c",
+          600: "#ca4a7d",
+          700: "#97375e",
+          800: "#65253e",
+          900: "#32121f"
+      },
+     
+      }
+
+    },
   },
   plugins: [],
 }
+
+
 
